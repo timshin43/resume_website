bdd32924c857ab209aa7c751611e8f4df5552da0
timshin43 <timshin.sergei@gmail.com>
2020-02-03 22:03:14 -0500

    updated all sections. 95%done. Switched static files to production

--- resume_website/settings.py
+++ resume_website/settings.py
@@ -143,9 +143,9 @@ AWS_DEFAULT_ACL= None
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/' #for the local machine
-# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATIC_URL = '/static/' #for the local machine
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
