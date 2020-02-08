61b2ec76211f6a58b335561ced9f42b3bf69cedd
timshin43 <timshin.sergei@gmail.com>
2020-02-08 12:55:08 -0500

    proofreading updates 1

--- resume_website/settings.py
+++ resume_website/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'http')
 SECURE_SSL_REDIRECT = True
 
 ROOT_URLCONF = 'resume_website.urls'
